59700bc72a541caa62c537ae5cb68597068cca51
lippegeleski <felippegeleski573@gmail.com>
2021-01-08 06:59:40 -0300

    first commit

---
+++ Estágio segunda/Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿
+namespace Estágio_segunda
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.lbColaboradores = new System.Windows.Forms.ListBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.tbColaborador = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbConvidados = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbConvidado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.llcomida = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbColaboradores
+            // 
+            this.lbColaboradores.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbColaboradores.FormattingEnabled = true;
+            this.lbColaboradores.Location = new System.Drawing.Point(473, 73);
+            this.lbColaboradores.Name = "lbColaboradores";
+            this.lbColaboradores.Size = new System.Drawing.Size(120, 208);
+            this.lbColaboradores.TabIndex = 0;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInserir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnInserir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnInserir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInserir.Location = new System.Drawing.Point(74, 418);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(301, 24);
+            this.btnInserir.TabIndex = 1;
+            this.btnInserir.Text = "Confirmar cadastro";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRemover.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnRemover.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnRemover.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemover.Location = new System.Drawing.Point(466, 312);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(301, 24);
+            this.btnRemover.TabIndex = 2;
+            this.btnRemover.Text = "Cancelar participação do churrasco";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // tbColaborador
+            // 
+            this.tbColaborador.Location = new System.Drawing.Point(77, 162);
+            this.tbColaborador.Name = "tbColaborador";
+            this.tbColaborador.Size = new System.Drawing.Size(290, 20);
+            this.tbColaborador.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(470, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Colaboradores:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Location = new System.Drawing.Point(74, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nome do colaborador:";
+            // 
+            // lbConvidados
+            // 
+            this.lbConvidados.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbConvidados.FormattingEnabled = true;
+            this.lbConvidados.Location = new System.Drawing.Point(642, 72);
+            this.lbConvidados.Name = "lbConvidados";
+            this.lbConvidados.Size = new System.Drawing.Size(120, 208);
+            this.lbConvidados.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(639, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Convidados:";
+            // 
+            // tbConvidado
+            // 
+            this.tbConvidado.Location = new System.Drawing.Point(77, 328);
+            this.tbConvidado.Name = "tbConvidado";
+            this.tbConvidado.Size = new System.Drawing.Size(290, 20);
+            this.tbConvidado.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Nome do convidado:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox1.Location = new System.Drawing.Point(219, 261);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(43, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Sim";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox2.Location = new System.Drawing.Point(331, 261);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 17);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "Não";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox3.Location = new System.Drawing.Point(219, 354);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(43, 17);
+            this.checkBox3.TabIndex = 14;
+            this.checkBox3.Text = "Sim";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox4.Location = new System.Drawing.Point(331, 354);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(46, 17);
+            this.checkBox4.TabIndex = 15;
+            this.checkBox4.Text = "Não";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(74, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Bebida:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(74, 358);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Bebida:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(74, 262);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Convidado:";
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox5.Location = new System.Drawing.Point(219, 195);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(43, 17);
+            this.checkBox5.TabIndex = 19;
+            this.checkBox5.Text = "Sim";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox6.Location = new System.Drawing.Point(331, 195);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(46, 17);
+            this.checkBox6.TabIndex = 20;
+            this.checkBox6.Text = "Não";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(571, 350);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 25;
+            // 
+            // llcomida
+            // 
+            this.llcomida.AutoSize = true;
+            this.llcomida.Location = new System.Drawing.Point(745, 350);
+            this.llcomida.Name = "llcomida";
+            this.llcomida.Size = new System.Drawing.Size(0, 13);
+            this.llcomida.TabIndex = 26;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(647, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Gasto com comida ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(647, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "Gasto com bebida";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(477, 360);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 55);
+            this.button3.TabIndex = 29;
+            this.button3.Text = "Total";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.lbConvidados);
+            this.panel1.Controls.Add(this.checkBox6);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.checkBox5);
+            this.panel1.Controls.Add(this.lbColaboradores);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.checkBox4);
+            this.panel1.Controls.Add(this.btnRemover);
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.btnInserir);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.tbColaborador);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.tbConvidado);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(853, 480);
+            this.panel1.TabIndex = 30;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(854, 480);
+            this.Controls.Add(this.llcomida);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormPrincipal";
+            this.Text = "Hammer Churrasco";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbColaboradores;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.TextBox tbColaborador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbConvidados;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbConvidado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label llcomida;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Estágio segunda/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estágio_segunda
+{
+
+    public partial class FormPrincipal : Form
+    {
+        List<string> colaboradores = new List<string>();
+        List<string> convidados = new List<string>();
+        public FormPrincipal()
+        {
+            InitializeComponent();
+
+            lbColaboradores.DataSource = colaboradores; //Carrega lista de nomes Colaboradores 
+            lbConvidados.DataSource = convidados;
+
+            checkBox2.Checked = true; //Já inicia com o checkBox do convidado marcado
+            checkBox5.Checked = true; //Já inicia com o checkBox da bebida do colaborado habilitado
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) //Checa o campo dos convidados
+            {
+                if (tbConvidado.Text == "" || !checkBox3.Checked && !checkBox4.Checked)
+                {
+                    MessageBox.Show("Preencha os campos.");
+                    tbColaborador.Focus();
+                    return;
+                }
+            }
+            if (tbColaborador.Text == "" && !! checkBox5.Checked || !! checkBox6.Checked) //Checa os campos do colaborador
+            {
+                MessageBox.Show("Preencha os campos.");
+                tbColaborador.Focus();
+                return;
+            }
+            //Coloca os participantes nas listas
+        
+                
+
+             colaboradores.Add(tbColaborador.Text);
+             tbColaborador.Clear();
+                
+             lbColaboradores.DataSource = null;
+             lbColaboradores.DataSource = colaboradores;
+
+             convidados.Add(tbConvidado.Text);
+             tbConvidado.Clear();
+                
+             lbConvidados.DataSource = null;
+             lbConvidados.DataSource = convidados;    
+            
+        }
+        private void btnRemover_Click(object sender, EventArgs e) //remove participantes
+        {
+            if (colaboradores.Count < 1)
+            {
+                MessageBox.Show("Não há nada para remover da lista.");
+                tbColaborador.Focus();
+                return;
+            }
+
+            colaboradores.RemoveAt(lbColaboradores.SelectedIndex);
+            lbColaboradores.DataSource = null;
+            lbColaboradores.DataSource = colaboradores;
+
+            convidados.RemoveAt(lbConvidados.SelectedIndex);
+            lbConvidados.DataSource = null;
+            lbConvidados.DataSource = convidados;
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) //Função para bloquear a área de convidado se não tiver um convidado
+        {
+            if (checkBox2.Checked)
+            {
+                tbConvidado.Enabled = false;
+                checkBox3.Enabled = false;
+                checkBox4.Enabled = false;
+                checkBox1.Checked = false;
+                tbConvidado.Clear();
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) //Libera a área de convidados
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox2.Checked = false;
+                tbConvidado.Enabled = true;
+                checkBox3.Enabled = true;
+                checkBox4.Enabled = true;
+                checkBox3.Checked = true;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e) //Código para CheckBox3 e 4, 5 e 6, não estarem marcados ao mesmo tempo 
+        {
+            if (checkBox3.Checked)
+            {
+                checkBox4.Checked = false;
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox5.Checked)
+            {
+                checkBox6.Checked = false;
+            }
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox6.Checked)
+            {
+                checkBox5.Checked = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //total de gastos com comida
+        {
+
+            int comidaSoma = 10;
+
+            if (checkBox1.Checked)
+            {
+                comidaSoma = 10 + 10;
+            }
+            comidaSoma = comidaSoma * colaboradores.Count;
+            
+            MessageBox.Show("O gasto com comida foi " + comidaSoma + "$.");
+            tbColaborador.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //total de gastos com bebida
+        {
+            int bebidaSoma = 0;
+
+            if (checkBox5.Checked)
+            {
+                bebidaSoma = bebidaSoma + 10;
+            }
+            if (checkBox1.Checked)
+            {
+                if(checkBox3.Checked)
+                {
+                    bebidaSoma = bebidaSoma + 10;
+                }
+            }
+            bebidaSoma = bebidaSoma * colaboradores.Count;
+
+            MessageBox.Show("O gasto com bebida foi " + bebidaSoma + "$.");
+            tbColaborador.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e) //total gasto
+        {
+            int bebidaSoma = 0;
+
+            if (checkBox5.Checked)
+            {
+                bebidaSoma = bebidaSoma + 10;
+            }
+            if (checkBox1.Checked)
+            {
+                if (checkBox3.Checked)
+                {
+                    bebidaSoma = bebidaSoma + 10;
+                }
+            }
+            bebidaSoma = bebidaSoma * colaboradores.Count;
+
+            int comidaSoma = 10;
+
+            if (checkBox1.Checked)
+            {
+                comidaSoma = 10 + 10;
+            }
+            comidaSoma = comidaSoma * colaboradores.Count;
+
+            int totalgasto = comidaSoma + bebidaSoma;
+
+            MessageBox.Show("O valor total foi " + totalgasto + "$.");
+            tbColaborador.Focus();
+        }
+    }
+}
